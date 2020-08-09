42340e53166f4f86f960899f48facf25b75694e7
vikashsingh3 <57470654+vikashsingh3@users.noreply.github.com>
2020-08-10 09:00:52 +1000

    Personal Portfolio Project - Initial commit

---
+++ personal_portfolio_project/portfolio/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=150)
+    image = models.ImageField(upload_to='portfolio/images/')
+    url = models.URLField(blank=True)
